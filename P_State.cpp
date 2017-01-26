2784e9c54e3376dcef594ac96dd437686a2fdba3
Justin Riddell <arghnews@hotmail.co.uk>
2017-01-26 09:38:59 +0000

    Simple physics state class

---
+++ P_State.cpp
@@ -0,0 +1,38 @@
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/rotate_vector.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/quaternion.hpp>
+
+#include "Util.hpp"
+#include "P_State.hpp"
+
+/* a simple class that encompasses the physical state of an object
+ * Things like it's position, momentum, mass and orientation are here
+*/
+
+P_State::P_State(v3 pos, float m) :
+    position(pos),
+    mass(m),
+    inverse_mass(1.0f/m) {
+    }
+
+void P_State::recalculate() {
+    velocity = momentum * inverse_mass;
+    acceleration = force * inverse_mass;
+}
+
+void P_State::add_force(const v3& f) {
+    forces.push_back(f);
+}
+
+v3 P_State::net_force() {
+    // sum of forces on object
+    v3 net(zeroV);
+    for (const auto& f: forces) {
+        net += f;
+    }
+    forces.clear();
+    return net;
+}
---
+++ P_State.hpp
@@ -0,0 +1,36 @@
+#ifndef P_STATE_HPP
+#define P_STATE_HPP
+
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/rotate_vector.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/quaternion.hpp>
+
+#include "Util.hpp"
+
+class P_State {
+    private:
+        vv3 forces;
+    public:
+        P_State(v3 pos, float m);
+        // primary
+        v3 position;
+        v3 momentum;
+        v3 force;
+        fq orient;
+
+        // secondary
+        v3 velocity;
+        v3 acceleration;
+
+        const float mass;
+        const float inverse_mass;
+
+        void recalculate();
+        void add_force(const v3& force);
+        v3 net_force();
+};
+
+#endif
