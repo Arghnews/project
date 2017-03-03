8458a2b487aa35e6fc3f7da90a35470ec545b748
Justin Riddell <arghnews@hotmail.co.uk>
2017-03-03 07:56:36 +0000

    Template for serialising/de& using cereal. Now to do a networked bit using asio and send that cereal shit

--- Archive.cpp
+++ Archive.cpp
@@ -1,14 +1,20 @@
 #include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
 #include <iostream>
 #include <sstream>
-#include <fstream>
 #include <string>
 #include "cereal/archives/binary.hpp"
-#include "cereal/details/traits.hpp"
+
+namespace cereal {
+    template<class Archive>
+        void serialize(Archive& archive, glm::vec3& v) {
+            archive(v.x, v.y, v.z);
+        }
+}
 
 struct Classy {
-    int x;
-    int y;
+   int x;
+   glm::vec3 y;
 
     template <class Archive>
     void serialize(Archive& archive) {
@@ -23,16 +29,19 @@ int main() {
 
     {
         cereal::BinaryOutputArchive oarchive(ss); // Create an output archive
-        Classy c1;
-        c1.x = 324;
-        oarchive(c1); // Write the data to the archive
+        Classy c1_in;
+        c1_in.x = 12.0f;
+        c1_in.y.x = 69.0f;
+        oarchive(c1_in); // Write the data to the archive
     } // archive goes out of scope, ensuring all contents are flushed
 
+    // insert networked magic here
+    
+    Classy c2_in;
     {
         cereal::BinaryInputArchive iarchive(ss); // Create an input archive
-        Classy c1_in;
-        iarchive(c1_in); // Read the data from the archive
-        std::cout << c1_in.x << "\n";
+        iarchive(c2_in); // Read the data from the archive
     }
+    std::cout << c2_in.x << "," << c2_in.y.x << "\n";
 
 }
